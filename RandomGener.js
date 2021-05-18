108df8c67d00bc8737e06798ede0715478d3bce0
Gian Ferro <84408753+gianluccaferro@users.noreply.github.com>
2021-05-18 15:12:24 -0500

    Primeira Versao

---
+++ RandomGener.js
@@ -0,0 +1,9 @@
+let nbr = Math.floor(Math.random()*10)
+let msg = ['good luck','good day','good week','let\'s work','get rich','bad day','rain today','show love','sunny day','rainy day']
+
+let rndMsg = () => {
+
+    return msg[nbr]
+}
+
+console.log(rndMsg)
